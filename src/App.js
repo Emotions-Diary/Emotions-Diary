67a1210d5a35772207dbb90ee8530e178b0a0470
lucy-pill <schmidtiana95@gmail.com>
2022-08-06 00:50:14 +0900

    add heroku

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-// CSS import
-import './App.css';
-
-// Router import
+// Package import
 import { Routes, Route } from 'react-router-dom';
 
 // Component import
+import SignIn from './pages/sign/SignIn'
+import SignUp from './pages/sign/SignUp'
 import Main from './pages/Main'
 import Write from './pages/Write'
 import Detail from './pages/Detail'
 
 function App() {
   return <Routes>
-    <Route path='/' exact="true" element={<Main />} />
+    {/* <Route path='/' exact="true" element={loginStatus ? <Main /> : <SignIn />} />  */}
+    <Route path='/' exact="true" element={<SignIn />} /> 
+    <Route path='/signup' exact="true" element={<SignUp />} />
+    <Route path='/main' exact="true" element={<Main />} />
     <Route path='/write' element={<Write />} />
     <Route path='/detail/:id' element={<Detail />} />
   </Routes>;
